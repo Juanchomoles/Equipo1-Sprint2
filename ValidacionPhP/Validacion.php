1c131e95555a5c56c7e40d2f137972567e4bbedf
Juanchomoles <119575624+Juanchomoles@users.noreply.github.com>
2023-11-07 12:00:30 +0100

    Agregando transacciones

--- ValidacionPhP/Validacion.php
+++ ValidacionPhP/Validacion.php
@@ -4,7 +4,6 @@ require 'Clases/Cliente.php';
 require 'Clases/ValidarParticular.php';
 require 'Clases/Particular.php';
 
-
 $name = $_POST['nombre'] ?? '';
 $lastName = $_POST['apellidos'] ?? '';
 $residence = $_POST['domicilio'] ?? '';
@@ -13,16 +12,58 @@ $phone = $_POST['phone'] ? intval($_POST['phone']) : 0;
 $dni = $_POST['dni'] ?? 0;
 $dniLetter = $_POST['dniLetter'] ?? '';
 $password = $_POST['contraseña'] ?? '';
+$tipo_usuario = "Particular";
+$rs = "razon social";
+
 $clienteP = new Particular(1, $name, $lastName, $residence, $dni, $phone, $email, $password);
-$particular = new ValidarParticular();
+$private = new ValidarParticular();
+try {
+    $pdo = new PDO("mysql:host=mysql-server;dbname=db_project_team1", "root", "secret");
+}
+catch (PDOException $e) {
+    die("Error en la conexión a la base de datos: " . $e->getMessage());
+}
 
 try {
-    $result = $particular->validateALL($clienteP);
-}
-catch(Exception $e){
+    $result = $private->validateALL($clienteP);
+
+
+    $pdo->beginTransaction();
+    $clientCreate = "INSERT INTO client (type_user, user_name, passwd, name, last_name, dni, address, email, phone, target_num, business_name) 
+    VALUES (:tipo_usuario,'',:password, :name,:lastname, :dni, :residence, :email, :phone, '', :rs)";
+
+    $client = $pdo->prepare($clientCreate);
+
+    $client->bindParam(':tipo_usuario', $tipo_usuario);
+    $client->bindParam(':name', $name);
+    $client->bindParam(':lastname', $lastName);
+    $client->bindParam(':residence', $residence);
+    $client->bindParam(':dni', $dni);
+    $client->bindParam(':phone', $phone);
+    $client->bindParam(':rs', $rs);
+    $client->bindParam(':email', $email);
+    $client->bindParam(':password', $password);
+
+    $client->execute();
+
+    $id = $pdo->lastInsertId();
+
+    $privateCreate = "INSERT INTO private (id) VALUES (:id)";
+
+    $private = $pdo->prepare($privateCreate);
+
+    $private->bindParam(':id', $id);
+
+    $private->execute();
+
+    $pdo->commit();
+
+} catch (PDOException $err) {
+    $pdo->rollBack();
+    echo "Error " . $err->getMessage();
+} catch (Exception $e) {
     $error = $e->getMessage();
 }
-
 ?>
 
 <html>
